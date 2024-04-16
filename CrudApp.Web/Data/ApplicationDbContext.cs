1d4b00512779fe0ddaf3ef36f91d26a07d17402e
Atahan <Atahan@DESKTOP-H62537A>
2024-04-16 23:39:36 +0300

    Add project files.

---
+++ CrudApp.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CrudApp.Web.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudApp.Web.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options) 
+        { 
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ CrudApp.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CrudApp.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
